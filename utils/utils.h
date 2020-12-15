c64c8cae719025325c1b254ebc047e2bddd59778
Zouheir Layine <zlayine@e2r5p5.1337.ma>
2020-12-15 17:15:00 +0100

    norme_utils

--- utils/utils.h
+++ utils/utils.h
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zlayine <zlayine@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/15 17:14:31 by zlayine           #+#    #+#             */
+/*   Updated: 2020/12/15 17:14:41 by zlayine          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef UTILS_H
 # define UTILS_H
 
-#include <unistd.h>
-#include <stdlib.h> 
+# include <unistd.h>
+# include <stdlib.h>
 
 int			ft_atoi(const char *str);
 void		ft_putstr(char *s);
